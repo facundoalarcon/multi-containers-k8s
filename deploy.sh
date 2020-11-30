6175984f9372920618624ed3fe08a3d585c2c530
alfa <facundoalarcon93@gmail.com>
2020-11-30 07:18:19 -0800

    deploy script changed

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t facundoalarcon/multi-client:latest -t facundoalarcon/multi-client:$SHA -f Dockerfile ./client
-docker build -t facundoalarcon/multi-server:latest -t facundoalarcon/multi-server:$SHA -f Dockerfile ./server
-docker build -t facundoalarcon/multi-worker:latest -t facundoalarcon/multi-worker:$SHA -f Dockerfile ./worker
+docker build -t facundoalarcon/multi-client:latest -t facundoalarcon/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t facundoalarcon/multi-server:latest -t facundoalarcon/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t facundoalarcon/multi-worker:latest -t facundoalarcon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push facundoalarcon/multi-client:latest
 docker push facundoalarcon/multi-server:latest
@@ -11,7 +11,6 @@ docker push facundoalarcon/multi-server:$SHA
 docker push facundoalarcon/multi-worker:$SHA
 
 kubectl apply -f k8s
-
 kubectl set image deployments/server-deployment server=facundoalarcon/multi-server:$SHA
 kubectl set image deployments/client-deployment client=facundoalarcon/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=facundoalarcon/multi-worker:$SHA
